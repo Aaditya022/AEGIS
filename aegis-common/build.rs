99809bd6bc485130ad29ef2c777e39c96c93477a
Aditya Agrawal <heyaditya022@gmail.com>
2026-07-26 14:35:59 +0530

    fix: proto_path should point to aegis/v1 directory, not proto root

--- aegis-common/build.rs
+++ aegis-common/build.rs
@@ -4,12 +4,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_client(true)
         .compile_protos(
             &[
-                "../proto/aegis/v1/common.proto",
-                "../proto/aegis/v1/sidecar.proto",
-                "../proto/aegis/v1/gateway.proto",
-                "../proto/aegis/v1/control.proto",
+                "common.proto",
+                "sidecar.proto",
+                "gateway.proto",
+                "control.proto",
             ],
-            &["../proto"],
+            &["../proto/aegis/v1"],
         )?;
     Ok(())
 }
